e6e788b1bc2def32b719de28f8d39b5a20b13beb
Aliaksandr Kazerski <Aliaksandr_Kazerski@epam.com>
2020-01-20 11:38:45 +0300

    feature/change news-form component

--- src/app/news/components/news-form/news-form.component.ts
+++ src/app/news/components/news-form/news-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { OneNewsModel } from '../../models/one-news.model';
 import { NewsApiService } from '../../services/news-api.service';
+
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-news-form',
@@ -11,20 +14,44 @@ import { NewsApiService } from '../../services/news-api.service';
 export class NewsFormComponent implements OnInit {
   oneNews: OneNewsModel;
 
-  constructor(private newsApiService: NewsApiService) { }
+  constructor(
+    private newsApiService: NewsApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
     this.oneNews = new OneNewsModel();
+
+    const observer = {
+      next: (oneNews: OneNewsModel) => {
+        this.oneNews = { ...oneNews};
+      },
+      error: (err: any) => console.log(err)
+    };
+
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) =>
+          this.newsApiService.getOneNews(params.get('oneNewsID'))
+        )
+      )
+      .subscribe(observer);
   }
 
   onSaveNews(): void {
     const oneNews = {...this.oneNews} as OneNewsModel;
+
     if (oneNews._id) {
       this.newsApiService.updateNews(oneNews);
     } else {
       this.newsApiService.createNews(oneNews);
     }
+
+    this.onGoBack();
   }
 
-  onGoBack(): void {}
+  onGoBack(): void {
+    this.router.navigate(['/news']);
+  }
 }
